3a6ed544dcf7364aa97b1391ecd187554da2f2d2
rollrat <rollrat.cse@gmail.com>
2022-07-04 14:54:17 +0900

    update

--- candy-web-ssr-pull.sh
+++ candy-web-ssr-pull.sh
@@ -1,4 +1,4 @@
+docker stop candywn && docker rm $_
 docker rmi $(docker images -q rollrat/candywn)
 docker pull rollrat/candywn:latest
-docker stop candywn && docker rm $_
 docker run -d --name candywn --network host rollrat/candywn:latest
