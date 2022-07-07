e267d9340952995b44df22e5cb92856da27a163d
rollrat <rollrat.cse@gmail.com>
2022-07-07 13:06:08 +0900

    update

--- candy-server-spring.sh
+++ candy-server-spring.sh
@@ -2,5 +2,5 @@ docker stop candyss && docker rm $_
 docker pull rollrat/candyss:latest
 docker rmi $(docker images -q rollrat/candyss)
 docker run -d -v /home/ec2-user/config-java:/config/ --env \
-  JAVA_TOOL_OPTIONS="-Xloggc:gc.log -XX:+PrintGCDetails -XX:+PrintTenuringDistribution -XX:+PrintGCTimeStamps -XX:+PrintGcDateStamps" \
+  JAVA_TOOL_OPTIONS="-Xlog:gc:gc.log -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGcDateStamps" \
   --name candyss --network host rollrat/candyss:latest
